eacf66d34d606442fdb2190bdea3ea2eac3743b5
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-02-28 11:11:46 +0530

    Create hours.c

---
+++ hours.c
@@ -0,0 +1,20 @@
+struct time
+{
+int time
+int minutes
+};
+int main()
+{
+printf("enter the time")
+scanf("%d,&time)
+printf("enter the minutes")
+scanf("%d,&minutes)
+}
+void main()
+{
+if(time>minutes)
+{
+--time
+print("time")
+}
+}
